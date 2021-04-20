1925e1389af7b5234e9d617782b0cf0ed0a4d742
Alejandro Leal <aleaberm12@gmail.com>
2021-04-20 17:04:01 +0200

    Evitar que la preview genere scroll extra

--- README.md
+++ README.md
@@ -1,22 +1,13 @@
 # VNDB Mod Extension Chrome
 
-![latest-version](https://img.shields.io/badge/Latest%20Version-0.1.0-brightgreen.svg)
+![latest-version](https://img.shields.io/badge/Latest%20Version-0.2.0-brightgreen.svg)
 
-## Pasos para instalar:
-
-> 1. Descargar o empaquetar la extensión*
-> 2. Ir a la sección de extensiones (chrome://extensions/), (vivaldi://extensions/)...
-> 3. Arrastar el archivo crx
-> 4. Pulsa añadir extensión
-
-> Si el navegador bloquea la extensión usar la opción 'Cargar descomprimida'
-
-## Pasos para empaquetar:
+## Instalar sin empaquetar
 
 > 1. Ir a la sección de extensiones (chrome://extensions/), (vivaldi://extensions/)...
-> 2. Click en "empaquetar extensión"
-> 3. Rellena el formulario de la modal
-> 4. Seguir los pasos de instalación
+> 2. Activa el modo desarrollador
+> 3. Hacer click en cargar descomprimida
+> 4. Selecciona el directorio del proyecto
 
 ## Previews
 
--- js/mainController.js
+++ js/mainController.js
@@ -1,21 +1,33 @@
 class MainController
 {
-    run()
-    {
+    run() {
         document.body.addEventListener('mouseover', this.handleMouseover, false);
     }
     
     handleMouseover(ev) {
+        // Para mejorar la posición respecto al mouse
+        const variation = 20;
+        const extraHeight = 169;
+        const extraWidth = 230;
+
         // Guardamos el elemento y la posición del mouse
         const targetElement = ev.target;
-        const mousePosition = {
-            x: ev.pageX + 20,
-            y: ev.pageY,
-        };
+        const mousePositionX = ev.pageX + variation;
+        const mousePositionY = ev.pageY;
+
+        // Comprobamos que el enlace de la preview es válido
         const isVisualNovelLink = targetElement.nodeName === 'A' && targetElement.href.includes('vndb.org/v');
 
-        // Buscamos la preview flotante, si no la creamos
-        let imgPreview = document.querySelector('#img-preview');
+        // Dimensiones de la web, para ajustar la posición de la preview
+        const { width: baseWidth, height: baseHeight } = document.querySelector("#maincontent").getBoundingClientRect();
+
+        const websiteHeight = baseHeight + extraHeight;
+        const websiteWidth = baseWidth + extraWidth;
+
+        // Eliminamos la preview existente
+        if (document.querySelector('#img-preview')) {
+            document.querySelector('#img-preview').remove();
+        }
     
         // Si hacemos hover en los enlaces mostramos la preview
         if (isVisualNovelLink) {
@@ -26,30 +38,43 @@ class MainController
                 fetch(targetElement.href, { cache: 'force-cache' })
                     .then(response => response.text())
                     .then(data => {
-                        if (!imgPreview) {
-                            imgPreview = document.createElement('DIV');
-                            imgPreview.id = 'img-preview';
-                            imgPreview.style.zIndex = 99999;
-                            imgPreview.style.position = 'absolute';
-                            imgPreview.style.cursor = 'not-allowed';
-    
-                            document.body.append(imgPreview);
-                        }
-    
-                        imgPreview.style.marginLeft = mousePosition.x + 'px';
-                        imgPreview.style.marginTop = mousePosition.y + 'px';
-    
                         // Obtenemos la preview de la ficha
                         const dom = new DOMParser().parseFromString(data, 'text/html');
                         const img = dom.documentElement.querySelector('.imghover--visible > img');
+
+                        // Si no conseguimos la preview no seguimos
+                        if (!img) {
+                            return;
+                        }
+
+                        const imgPreview = document.createElement('DIV');
+                        imgPreview.id = 'img-preview';
+                        imgPreview.style.zIndex = 99999;
+                        imgPreview.style.position = 'absolute';
+                        imgPreview.style.cursor = 'not-allowed';
     
-                        imgPreview.innerHTML = img ? img.outerHTML : '';
+                        document.body.append(imgPreview);
+    
+                        imgPreview.style.marginLeft = mousePositionX + 'px';
+                        imgPreview.style.marginTop = mousePositionY + 'px';
+    
+                        imgPreview.innerHTML = img.outerHTML;
+
+                        // Si la preview no tiene espacio ajustamos su posición
+                        const { width: imgWidth, height: imgHeight } = imgPreview.getBoundingClientRect();
+
+                        const finalImgX = imgWidth + mousePositionX;
+                        const finalImgY = imgHeight + mousePositionY;
+
+                        if (websiteWidth && websiteWidth < finalImgX) {
+                            imgPreview.style.marginLeft = (websiteWidth - imgWidth - variation) + 'px';
+                        }
+
+                        if (websiteHeight && websiteHeight < finalImgY) {
+                            imgPreview.style.marginTop = (websiteHeight - imgHeight - variation) + 'px';
+                        }
                     });
             }, 100);
-        }
-
-        if(!isVisualNovelLink && imgPreview) {
-            imgPreview.innerHTML = '';
         }
     }
 }
